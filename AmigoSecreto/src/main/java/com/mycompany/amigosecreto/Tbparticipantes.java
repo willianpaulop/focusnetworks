0016bdbd5e66dabdc070e0b3444b692acc9928b3
willianpaulop <willian.p.paula@hotmail.com>
2015-06-16 11:59:15 -0300

    Commit
    H2 Database
    DROP TABLE IF EXISTS TBPARTICIPANTES;
    CREATE TABLE TBPARTICIPANTES(ID INT PRIMARY KEY AUTO_INCREMENT, NAME
    VARCHAR(255), EMAIL VARCHAR(255);

---
+++ AmigoSecreto/src/main/java/com/mycompany/amigosecreto/Tbparticipantes.java
@@ -0,0 +1,104 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.amigosecreto;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author Gotts
+ */
+@Entity
+@Table(name = "TBPARTICIPANTES")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Tbparticipantes.findAll", query = "SELECT t FROM Tbparticipantes t"),
+    @NamedQuery(name = "Tbparticipantes.findById", query = "SELECT t FROM Tbparticipantes t WHERE t.id = :id"),
+    @NamedQuery(name = "Tbparticipantes.findByName", query = "SELECT t FROM Tbparticipantes t WHERE t.name = :name"),
+    @NamedQuery(name = "Tbparticipantes.findByEmail", query = "SELECT t FROM Tbparticipantes t WHERE t.email = :email")})
+public class Tbparticipantes implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "ID")
+    private Integer id;
+    @Size(max = 255)
+    @Column(name = "NAME")
+    private String name;
+    // @Pattern(regexp="[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?", message="Invalid email")//if the field contains email address consider using this annotation to enforce field validation
+    @Size(max = 255)
+    @Column(name = "EMAIL")
+    private String email;
+
+    public Tbparticipantes() {
+    }
+
+    public Tbparticipantes(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Tbparticipantes)) {
+            return false;
+        }
+        Tbparticipantes other = (Tbparticipantes) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.mycompany.amigosecreto.Tbparticipantes[ id=" + id + " ]";
+    }
+    
+}
